bcdca9994b21ae5d68eaf0e24f8c01a0fbe5fb75
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-07-04 19:42:36 +0200

    First commit

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -13,7 +13,7 @@ const Ecommerce = () => {
 					<div className="flex justify-between items-center">
 						<div>
 							<p className="font-bold text-gray-400">Earnings</p>
-							<p className="text-2xl">$63,448.78</p>
+							<p className="text-2xl">$63,548.78</p>
 						</div>
 						<div className="mt-6">
 							<Button color="white" bgColor="blue" text="Download" borderRadius="10px" size="md" />
